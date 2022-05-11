26e7ecb16666c38f63213b4cefdf0f5a74c749ef
ireck22 <dragon9029@gmail.com>
2022-05-11 16:25:06 +0800

    上傳day04

---
+++ Day04#01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+import sys
+import json
+import pandas as pd
+import numpy as np
+import codecs
+import re
+
+
+def add1(n):
+    return n+1
+
+def isPrime(n):
+    if n%2==1:
+        return "true"
+    else:
+        return "false"
+    
+def f(L, F):
+    result=[]
+    for i in L:
+        result.append(F(i))
+    return result
+
+L=[1,2,3,4,5,6]
+print(L)
+F=add1
+L2=f(L,F)          #帶入要處理的陣列和function
+print("每個元素加1: ",L2)
+
+F=isPrime
+L2=f(L,F)          #帶入要處理的陣列和function
+print("判斷是否為質數: ",L2)
+
---
+++ Day04#02.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+from ast import Str
+import sys
+import json
+import pandas as pd
+import numpy as np
+import codecs
+import re
+
+strss='Here are UPPERCASE and lowercase chars'
+finish_result={}
+x=1
+
+#========== 計算每個字母出現位置 start==========
+for i in strss:
+    if i in finish_result: 
+        finish_result[i]+=','+str(x)
+    else:
+        finish_result[i]=str(x)
+    x+=1
+#========== 計算每個字母出現位置 end===========
+
+print(finish_result)
